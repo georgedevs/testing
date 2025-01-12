71887aeca9224bfead5e02d547fc11eba9de2530
georgedevs <gukohgodwin@gmail.com>
2025-01-12 19:08:44 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,14 @@
 import LandingPage from './pages/LandingPage';
 import RelationshipCards from './pages/RelationshipCard';
 import AfterCard from './pages/AfterCard';
-import TestimonialSection from './pages/TestimonialSection';
 import AfterTestimonial from './pages/AfterTestimonial';
 import Footer from './pages/Footer';
 import Heading from '@/components/Heading';
 
 export default function Home() {
   return (
-    <>
-     <Heading
+    <div className="relative w-full overflow-x-hidden">
+      <Heading
         title='MiCounselor'
         description='MiCounselor is a platform that provides anonymous marriage and relationship counseling'
         keywords='Counseling, Therapy, MiCounselor, Marriage'
@@ -20,6 +19,6 @@ export default function Home() {
       <AfterCard />
       <AfterTestimonial />
       <Footer />
-    </>
+    </div>
   );
 }
