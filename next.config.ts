78b1a5a5f822351d29dc7b78757f726e91688bee
georgedevs <gukohgodwin@gmail.com>
2025-01-14 23:41:22 +0100

    fixed bug

--- next.config.ts
+++ next.config.ts
@@ -21,12 +21,12 @@ const nextConfig: NextConfig = {
   },
   async rewrites() {
     return [
-        {
-            source: '/api/:path*',
-            destination: '${process.env.NEXT_PUBLIC_SERVER_URL}/:path*',
-        },
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/:path*`,  // Changed to backticks
+      },
     ];
-},
+  },
 };
 
 export default nextConfig;
